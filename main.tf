dc888f411b3a079b6bfbfca1fb1d44ceb50ad391
bayupw <bayupw@gmail.com>
2022-02-08 22:31:54 +1300

    first commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+resource "aws_iam_role" "this" {
+  name               = "ssm-instance-profile-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": {
+    "Effect": "Allow",
+    "Principal": {"Service": "ec2.amazonaws.com"},
+    "Action": "sts:AssumeRole"
+  }
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  role       = aws_iam_role.this.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "this" {
+  name = "ssm-instance-profile"
+  role = aws_iam_role.this.name
+}
